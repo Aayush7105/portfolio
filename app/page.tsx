89264c6174a233322254408444c671b8226e0177
Aayush7105 <aayushrawat5107@gmail.com>
2026-01-25 15:55:59 +0530

    code updated

--- app/page.tsx
+++ app/page.tsx
@@ -4,13 +4,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    // <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-neutral-950">
-    //   <h1 className="mx-auto max-w-3xl bg-linear-to-br from-red-300 via-neutral-300 via-50% to-red-300/30 bg-clip-text py-2 text-center text-5xl leading-relaxed font-medium tracking-tighter text-balance text-transparent md:text-6xl filter: blur(0px); opacity: 1; transform: none;">
-    //     <span className="block leading-[1.1]">Design Bold.</span>
-    //     <span class="block leading-[1.1]">Ship Seamless.</span>
-    //   </h1>
-    // </div>
-    <div className="min-h-screen bg-neutral-100 dark:bg-neutral-950 relative overflow-hidden border border-dotted border-neutral-300/80 ">
+    <div className="min-h-screen bg-neutral-100 dark:bg-neutral-950 relative overflow-hidden border border-dotted border-neutral-300/80">
       <div
         className="absolute inset-1 
     dark:bg-[radial-gradient(#ffffff1A_1px,transparent_1px)]
@@ -25,10 +19,10 @@ export default function Home() {
       border border-neutral-400/50
       dark:absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#4f4f4f1A_1px,transparent_1px),linear-gradient(to_bottom,#8080801A_1px,transparent_1px)] bg-size-[14px_24px] flex justify-center items-center  dark:border-neutral-700/50"
           >
-            <div className="h-screen  w-[85%] border border-dashed border-neutral-600/40 dark:border-neutral-500/50 py-16  ">
-              <div className="border-b border-t border-dashed border-neutral-600/40 dark:border-neutral-500/50 flex justify-between">
+            <div className="min-h-screen  sm:w-[90%] md:w-[85%] border border-dashed border-neutral-600/40 dark:border-neutral-500/50 sm:py-16  ">
+              <div className="border-b border-t border-dashed border-neutral-600/40 dark:border-neutral-500/50 flex justify-between sm:flex-row">
                 <div className="w-[50%] flex">
-                  <div className="w-[35%] border-r border-dashed border-neutral-500/50">
+                  <div className="sm:w-[35%] border-r border-dashed border-neutral-500/50">
                     <Image
                       src={"/images.jpeg"}
                       alt="sjx"
