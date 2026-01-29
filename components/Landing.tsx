13540aa6811264e4139136b492535d373e85e96e
Aayush7105 <aayushrawat5107@gmail.com>
2026-01-29 21:19:20 +0530

    added github

--- components/Landing.tsx
+++ components/Landing.tsx
@@ -7,7 +7,7 @@ import Projects from "./Project";
 const Landing = () => {
   return (
     <div className=" h-full w-full max-w-5xl flex justify-center items-center  bg-white dark:bg-neutral-950  ">
-      <div className="h-full sm:w-[90%] md:w-[85%] ring ring-dashed ring-neutral-500/40    dark:ring-neutral-500/50 sm:py-16  flex flex-col ">
+      <div className="h-full sm:w-[90%] md:w-[85%] ring ring-dashed ring-neutral-500/40 dark:ring-neutral-500/50 sm:py-16  flex flex-col ">
         <Hero />
         <Aboutme />
         <Contactme />
